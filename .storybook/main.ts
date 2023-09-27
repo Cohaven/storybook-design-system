5f2cdbd1c85591e51671f1459876c2059a82b70c
Artemy Matvienko <artemy.matvienko@fiixsoftware.com>
2023-09-27 10:16:51 -0400

    install addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,22 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+    'storybook-addon-pseudo-states',
+    '@chromaui/addon-visual-tests',
+    {
+      name: '@chromaui/addon-visual-tests',
+      options: {
+        projectId: 'Project:6511db327b8c8fa710d60d18',
+        projectToken: 'chpt_6a460545677e332',
+      },
+    },
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
